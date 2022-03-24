e577fcafb740c7ac74f9843e496dc886694fa34c
Devanshu536 <102072128+Devanshu536@users.noreply.github.com>
2022-03-24 16:32:23 +0530

    Update authentication.js

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -3,13 +3,46 @@ const bcrypt = require('bcrypt');
 const student = require('../models/student');
 
 exports.login = function (req, res) {
-    // check credentials and regenerate session if matched
+    app.use(session({
+        cookie: {
+            httpOnly: true,
+            maxAge: millisecondsInTwoDays,
+            path: '/api'
+        },
+        name: 'sessionId',
+        resave: false,
+        rolling: true,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    }));
 };
 
 exports.logout = function (req, res) {
-    // destroy session
+    app.use(session({
+        cookie: {
+            httpOnly: true,
+            maxAge: millisecondsInTwoDays,
+            path: '/api'
+        },
+        name: 'sessionId',
+        resave: false,
+        rolling: true,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    }));
 };
 
 exports.authenticate = function (req, res, next) {
-    // check if session is authenticated
+    app.use(session({
+        cookie: {
+            httpOnly: true,
+            maxAge: millisecondsInTwoDays,
+            path: '/api'
+        },
+        name: 'sessionId',
+        resave: false,
+        rolling: true,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    }));
 };
